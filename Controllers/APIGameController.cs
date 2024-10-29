9062894f55a8680ed174e84d0d012a237be3d663
skwaii00 <shuu0597@gmail.com>
2024-10-29 15:45:22 +0700

    Add project files.

---
+++ Controllers/APIGameController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SeverGame106.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class APIGameController : ControllerBase
+    { 
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Game106Model game106 = new Game106Model
+            {
+                CourseName = "Back-End Game Programming",
+                CourseCode = "GAM106",
+                Name = "Dinh Van Binh",
+                StudentCode = "PD10121",
+                Class = "GAM19301"
+            };
+            int status = 1;
+            string message = "Get data sucess";
+            var data = new { status, message, game106 };
+            return new JsonResult(game106);
+        }
+    }
+}
---
+++ Models/Class.cs
@@ -0,0 +1,11 @@
+﻿namespace SeverGame106.Models
+{
+    public class Game106Model
+    {
+        public string? CourseName { get; set; }
+        public string? CourseCode { get; set; }
+        public string? Name { get; set; }
+        public string? StudentCode { get; set; }
+        public string? Class { get; set; }
+    }
+}
